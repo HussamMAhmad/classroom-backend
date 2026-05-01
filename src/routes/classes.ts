667013bb07f36897843a70c969ab402c5b1025aa
Hussam <nirolive134@gmail.com>
2026-05-01 20:42:40 +0300

    implement get classes

--- src/routes/classes.ts
+++ src/routes/classes.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   try {
     const { ...classe } = req.body;
-    console.log('classes is : ' , classe);
+    console.log("classes is : ", classe);
     const classeCreate = await prisma.classes.create({
       data: {
         name: classe.name,
@@ -35,6 +35,78 @@ router.post("/", async (req, res) => {
     res
       .status(500)
       .json({ message: "somthing wrong with create a new class", error: e });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const { search, limit = "10", page = "1" } = req.query;
+
+    const currentLimit = Number(limit);
+    const currentPage = Number(page);
+
+    console.log("the limit is : " , currentLimit);
+    console.log("the page is : " , currentPage); 
+
+    if (
+      currentPage < 1 ||
+      !Number.isInteger(currentPage) ||
+      currentLimit < 1 ||
+      !Number.isInteger(currentLimit)
+    ) {
+      return res.status(403).json({
+        message: "page and limit must be positive integer",
+      });
+    }
+
+    const limitPerPage = Math.min(currentLimit);
+
+    const offSet = (currentPage - 1) * limitPerPage;
+
+    let whereClause: any = {};
+
+    if (search) {
+      const searchQuery = String(search);
+      whereClause = {
+        name: { contains: searchQuery, mode: "insensitive" },
+      };
+    }
+    const [result, count] = await Promise.all([
+      prisma.classes.findMany({
+        where: whereClause,
+        skip: offSet,
+        take: limitPerPage,
+        orderBy: {
+          createdAt: "desc",
+        },
+        include: {
+          subject: true,
+          teacher: true,
+        },
+      }),
+      prisma.classes.count({
+        where: whereClause,
+      }),
+    ]);
+
+    const totalCount = count ? count : 0;
+
+    res.status(200).json({
+      message: "Classes retrieved successfully",
+      data: result,
+      pagination: {
+        page: currentPage,
+        limit: limitPerPage,
+        pageSize: totalCount,
+        totalPages: Math.ceil(totalCount / limitPerPage),
+      },
+    });
+  } catch (e) {
+    console.log("faild to get data : ", e);
+    res.status(403).json({
+      message: "something went wrong with getting classes",
+      errore: e,
+    });
   }
 });
 
