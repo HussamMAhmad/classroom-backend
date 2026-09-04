4ce4c7686725eb89fa091168634c58034923075b
Hussam <nirolive134@gmail.com>
2026-09-04 11:58:31 +0300

    disable arcjet function

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
-app.use(securityMiddleware);
+// app.use(securityMiddleware);
 
 app.use("/api/users", usersRouter);
 app.use("/api/subjects", SubjectRouter);
